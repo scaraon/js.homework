40f76e704d2a7d0665d2134fe46e3a554dc4627b
MacBook <macbook@MacBooks-MacBook-Air.local>
2023-11-23 18:21:09 +0600

    dz

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ if (total > ordered) {
 } else {
   console.log("На складе недостаточно товаров!");
 }
+
+const password = prompt("Введите пароль:");
+const ADMIN_PASSWORD = "jqueryismyjam";
+
+if (password == null) {
+  alert("Отменено пользователем!");
+} else if (password === ADMIN_PASSWORD) {
+  alert("Добро пожаловать!");
+} else {
+  alert("Доступ запрещен, неверный пароль!");
+}
+
+const credits = 23580;
+const pricePerDroid = 3000;
+const price = prompt("Введите количество дроидов которые хотите купить");
+const totalPrice = pricePerDroid * price;
+const sum = credits - totalPrice;
+
+if (price == null) {
+  alert("Отменено пользователем!");
+} else if (totalPrice > credits) {
+  alert("Недостаточно средств на счету!");
+} else if (totalPrice <= credits) {
+  alert(`Вы купили ${price} дроидов, на счету осталось ${sum} кредитов.`);
+}
